2833d91702cdd6c27562c7682ad84c786dd295c6
Jack Dehlin <jdehlin@gmail.com>
2014-05-12 14:18:19 -0400

    Remember users chat sessions across page loads

--- src/HipChat.Widget.Mvc/WidgetManager.cs
+++ src/HipChat.Widget.Mvc/WidgetManager.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Web;
+using System.Web.UI.WebControls;
+using HipChat.Widget.Mvc.Entities;
 
 namespace HipChat.Widget.Mvc
 {
     public static class WidgetManager
     {
-        public static Widget SetupRoom(string username)
+        public static Widget SetupRoom(string username = "Guest")
         {
             // check if it's a robot
             if (HttpContext.Current.Request.UserAgent != null 
@@ -18,9 +20,12 @@ namespace HipChat.Widget.Mvc
  
             // setup hipchat client    
             var client = new HipChatApi(Settings.AuthToken);
- 
+
+            // get a list of the rooms
+            var rooms = client.RoomsList().RoomList;
+
             // check if at least 1 staff is online
-            var notifyRoom = client.RoomsList().RoomList.FirstOrDefault(r => r.Name == Settings.NotifyRoom);
+            var notifyRoom = rooms.FirstOrDefault(r => r.Name == Settings.NotifyRoom);
             if (notifyRoom == null)
                 throw new Exception("There's no room to notify");
             notifyRoom = client.RoomsShow(notifyRoom.Id);
@@ -29,17 +34,27 @@ namespace HipChat.Widget.Mvc
                 throw new Exception("We're offline");
     
             // setup room name
-            var roomName = string.Format("{0} {1} {2}", Settings.RoomNamePrefix, username, new Random().Next(10000));
-            HttpContext.Current.Response.Cookies.Add(new HttpCookie("hipchat_room", roomName));
+            var roomNameCookie = HttpContext.Current.Request.Cookies.Get("HipChatSupportRoom");
+            var roomName = roomNameCookie != null ? roomNameCookie.Value : string.Empty;
+            var room = rooms.FirstOrDefault(r => r.Name == roomName);
+            if (string.IsNullOrWhiteSpace(roomName) || room == null)
+            {
+                if (username == "Guest")
+                    roomName = string.Format("{0} {1} {2}", Settings.RoomNamePrefix, username, new Random().Next(10000));
+                else 
+                    roomName = string.Format("{0} {1}", Settings.RoomNamePrefix, username);
+            }
+            HttpContext.Current.Response.Cookies.Add(new HttpCookie("HipChatSupportRoom", roomName));
 
-            // create room
-            var room = client.RoomsCreate(roomName, Settings.OwnerUserId, guestAccess: true);
+            // create room if necessary
+            if (room == null)
+                room = client.RoomsCreate(roomName, Settings.OwnerUserId, guestAccess: true);
             room = client.RoomsShow(room.Id);
-
+            
             // notify administrators
             if (room.Partcipants.UserList == null || room.Partcipants.UserList.All(p => users.All(u => u.Id != p.Id)))
                 client.RoomsMessage(notifyRoom.Id, username, string.Format("{0} wants to chat, please join room '{1}'", username, room.Name), true);
-
+            
             return new Widget(room);
         }
 
